f6f2a97327dddfbc47e16b04df8f16c3d501e0e9
Mark Levison <mark@agilepainrelief.com>
2024-10-17 16:05:14 -0400

    Normalize parsing of BigDecimal so that all amounts are nnnn.nn

--- src/main/java/org/example/TransactionData.java
+++ src/main/java/org/example/TransactionData.java
@@ -3,6 +3,8 @@ package org.example;
 import org.apache.commons.csv.CSVRecord;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -14,17 +16,20 @@ public class TransactionData {
 
     public TransactionData(CSVRecord record) {
         store = record.get(1);
-        date  = LocalDate.parse(record.get(0), dateTimeFormatter);
+        date = LocalDate.parse(record.get(0), dateTimeFormatter);
 
         String tmpAmount = record.get(2);
 
         if (tmpAmount.isEmpty()) {
-            tmpAmount = "-"+record.get(3);
+            tmpAmount = "-" + record.get(3);
         }
-        amount = new BigDecimal(tmpAmount).negate();
-
-
-
+        DecimalFormat decimalFormat = new DecimalFormat("#,###.##");
+        decimalFormat.setParseBigDecimal(true);
+        try {
+            amount = ((BigDecimal) decimalFormat.parse(tmpAmount)).setScale(2).negate();
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getStore() {
--- src/test/java/TDCreditLoaderTest.java
+++ src/test/java/TDCreditLoaderTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -103,22 +102,22 @@ public class TDCreditLoaderTest {
     private final BigDecimal[] amountList = {
             new BigDecimal("-18074").movePointLeft(2),
             new BigDecimal("-172").movePointLeft(2),
-            new BigDecimal("-85"),
+            new BigDecimal("-8500").movePointLeft(2),
             new BigDecimal("-3818").movePointLeft(2),
             new BigDecimal("-712").movePointLeft(2),
             new BigDecimal("-735").movePointLeft(2),
             new BigDecimal("-199").movePointLeft(2),
             new BigDecimal("-2731").movePointLeft(2),
-            new BigDecimal("-85").movePointLeft(1),
-            new BigDecimal("-85").movePointLeft(1),
+            new BigDecimal("-850").movePointLeft(2),
+            new BigDecimal("-850").movePointLeft(2),
             new BigDecimal("-3572").movePointLeft(2),
             new BigDecimal("-524").movePointLeft(2),
             new BigDecimal("-1095").movePointLeft(2),
-            new BigDecimal("-192").movePointLeft(1),
+            new BigDecimal("-1920").movePointLeft(2),
             new BigDecimal("-1594").movePointLeft(2),
             new BigDecimal("-2033").movePointLeft(2),
             new BigDecimal("-6183").movePointLeft(2),
-            new BigDecimal("-85"),
+            new BigDecimal("-8500").movePointLeft(2),
             new BigDecimal("-2069").movePointLeft(2),
             new BigDecimal("-850").movePointLeft(2),
             new BigDecimal("-1929").movePointLeft(2),
@@ -154,10 +153,9 @@ public class TDCreditLoaderTest {
         for (CSVRecord row : rows) {
             TransactionData transactionRow = new TransactionData(row);
             int rowNumber = (int) row.getRecordNumber() - 1;
- //           assertThat(transactionRow.getStore()).withFailMessage("Actual Store: " + transactionRow.getStore() + " Expected: " + storeList[rowNumber] + " at row: " + rowNumber).isEqualTo(storeList[rowNumber]);
+            //           assertThat(transactionRow.getStore()).withFailMessage("Actual Store: " + transactionRow.getStore() + " Expected: " + storeList[rowNumber] + " at row: " + rowNumber).isEqualTo(storeList[rowNumber]);
 
             assertThat(transactionRow.getDate()).withFailMessage("Actual Date: " + transactionRow.getDate() + " Expected: " + dateList[rowNumber] + " at row: " + rowNumber).isEqualTo(dateList[rowNumber]);
-
 
             assertThat(transactionRow.getAmount()).withFailMessage("Actual Amount: " + transactionRow.getAmount() + " Expected: " + amountList[rowNumber] + " at row: " + rowNumber).isEqualTo(amountList[rowNumber]);
         }
